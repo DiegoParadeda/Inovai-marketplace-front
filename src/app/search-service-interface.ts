2f2589e98b36756ce365d0507c47ea1bfb8acc77
Diego Paradeda <diegoparadeda@localhost.localdomain>
2018-08-31 16:24:44 -0300

    [ADD] SearchServiceInterface add sort & order

--- src/app/search-service-interface.ts
+++ src/app/search-service-interface.ts
@@ -1,5 +1,5 @@
 import {Observable} from 'rxjs/Observable';
 
 export interface SearchServiceInterface {
-    list(max: number, offset: number, sort?: any, search?: any): Observable<any>;
+    list(max: number, offset: number, sort?: any, order?: any, search?: any): Observable<any>;
 }
